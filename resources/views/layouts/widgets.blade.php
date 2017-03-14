9627c21c7b26872d0fe60ea62d47b1e441f55b6e
Jason D. Moss <jason@jdmlabs.com>
2017-03-14 14:34:04 -0300

    Reset

--- resources/views/layouts/widgets.blade.php
+++
@@ -1,6 +0,0 @@
-@include('widget.user')
-@if(AenginusConfig::getValue('enable_hot_posts') == 'true')
-    @include('widget.hot_posts')
-@endif
-@include('widget.categories')
-@include('widget.tags')
--- resources/views/mail/welcome.blade.php
+++
@@ -1,6 +0,0 @@
-<!DOCTYPE html>
-<html lang="en">
-<body>
-<a class="btn btn-success" href="https://lufficc.com">Welcome to my website</a>
-</body>
-</html>
